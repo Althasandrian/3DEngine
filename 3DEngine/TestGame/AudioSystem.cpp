9cb5db9ad31d91d4736bd000e410fb23f1c44f79
FroznShadow <tonilyy@AOTA1-1LDML-09D.kamit.fi>
2016-02-22 09:41:25 +0200

    audio

--- 3DEngine/TestGame/AudioSystem.cpp
+++ 3DEngine/TestGame/AudioSystem.cpp
@@ -1,9 +1,27 @@
 #include "AudioSystem.h"
 
-AudioSystem::AudioSystem()
+AudioSystem::AudioSystem(irrklang::ISoundEngine* engine)
 {
+	soundEngine = engine;
 }
 
 AudioSystem::~AudioSystem()
 {
+	soundEngine->drop();
+}
+void AudioSystem::audioPlay()
+{
+	soundEngine->play3D(soundSource, position);
+}
+void AudioSystem::audioPause()
+{
+	
+}
+void AudioSystem::includeAudio(std::string filepath)
+{
+	soundSource = soundEngine->addSoundSourceFromFile(filepath.c_str());
+}
+void AudioSystem::setPosition(irrklang::vec3df pos)
+{
+	position = pos;
 }
