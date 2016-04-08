02fe55bb652481c3e0d0ab33564439a488e3f0b5
FroznShadow <tonilyytikainen@kamk.fi>
2016-04-08 13:05:09 +0300

    scenesystem prototype

--- 3DEngine/3DEngine/SceneSystem.h
+++ 3DEngine/3DEngine/SceneSystem.h
@@ -8,17 +8,28 @@
 #ifndef SCENESYSTEM_H
 #define SCENESYSTEM_H
 
+#include "Scene.h"
+
 class SceneSystem
 {
 public:
-	//Scene
+	
+	SceneSystem(){}
+	~SceneSystem(){}
+
+	Scene* getCurrentScene();
+	void changeScenes(int id);
+	void addScene(Scene* scene);
+	void loadScene(int id);
+	void unLoadScene(int id );
+
 
 protected:
 
 private:
-	SceneSystem(){}
-	~SceneSystem(){}
-
+	std::vector<Scene*> sceneList;
+	Scene* currentScene;
+	void setScene(Scene* scene);
 
 };
 
